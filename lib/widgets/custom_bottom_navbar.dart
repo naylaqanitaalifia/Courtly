942e7e63578a9190ea91551a9331b916a8db43c4
naylaqanitaalifia <naylaqanita286@gmail.com>
2026-04-25 22:59:15 +0700

    first commit

---
+++ lib/widgets/custom_bottom_navbar.dart
@@ -0,0 +1,11 @@
+import "package:flutter/material.dart";
+
+class CustomBottomNavbar extends StatelessWidget {
+  @override
+
+  Widget build (BuildContext context) {
+    return Scaffold (
+      bottomNavigationBar: 
+    )
+  }
+}
